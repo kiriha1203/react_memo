7286597f2d8122d6c5c28fd824264c351fe48619
kiriha1203 <mizumoto.takefumi@gmail.com>
2020-10-12 21:48:15 +0900

    memo

---
+++ src/memo/Store.js
@@ -0,0 +1,100 @@
+import { createStore } from 'redux';
+
+const initData = {
+  data:[{message:'sample data', created:new Date()}],
+  message: 'please type message:',
+  mode: 'default',
+  fdata:[]
+};
+
+export function memoReducer(state = initData, action) {
+  switch (action.type) {
+    case 'ADD':
+      return addReduce(state, action);
+
+    case 'DELETE':
+      return deleteReduce(state, action);
+    
+    case 'FIND':
+      return findReduce(state, action);
+
+    default:
+      return state;
+  }
+}
+
+//ReduceAction
+
+//Add
+function addReduce(state, action){
+  let data = {
+    message:action.message,
+    created:new Date()
+  };
+  let newdata = state.data.slice();
+  newdata.unshift(data);
+  return {
+    data:newdata,
+    message:'Added!',
+    mode:'default',
+    fdata:[]
+  };
+}
+
+//Search
+function findReduce(state, action){
+  let f = action.find;
+  let fdata = [];
+  state.data.forEach((value)=>{
+    if (value.message.indexOf(f) >= 0){
+      fdata.push(value);
+    }
+  });
+  return {
+    data:state.data,
+    message:'find "' + f + '":',
+    mode:'find',
+    fdata:fdata
+  };
+}
+
+//Delete
+function deleteReduce(state, action){
+  let newdata = state.data.slice();
+  newdata.slice(action.index, 1);
+  return {
+    data:newdata,
+    message:'delete "' + action.index + '":',
+    mode:'delete',
+    fdata:[]
+  }
+}
+
+//Action Creater
+
+// Add Action
+export function addMemo(text) {
+  return {
+    type: 'ADD',
+    message:text
+  }
+}
+
+// Delete Action
+export function deleteMemo(num) {
+  return {
+    type: 'DELETE',
+    index:num
+  }
+}
+
+// Search Action
+export function findMemo(text) {
+  return {
+    type: 'FIND',
+    find:text
+  }
+}
+
+// Create Store
+export default createStore(memoReducer);
