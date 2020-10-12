41a6c78f19bff509323b6bc4946aa15666410278
kiriha1203 <mizumoto.takefumi@gmail.com>
2020-10-12 23:20:19 +0900

    add persist

--- src/index.js
+++ src/index.js
@@ -1,14 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
 import { Provider } from 'react-redux';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import { PersistGate } from 'redux-persist/integration/react';
 import './index.css';
 import App from './App';
-import MemoStore from './memo/Store'
+import MemoStore, { memoReducer } from './memo/Store';
 import * as serviceWorker from './serviceWorker';
 
+// redux persist
+const persistConfig = {
+  key: 'memo',
+  storage,
+  blacklist: ['message', 'mode', 'fdata'],
+  whitelist: ['data']
+};
+
+// create persistreducer
+const persistedReducer = persistReducer(persistConfig, memoReducer);
+
+//create store and persister
+let store = createStore(persistedReducer);
+let pstore = persistStore(store);
+
 ReactDOM.render(
-  <Provider store={MemoStore}>
-    <App />
+  <Provider store={store}>
+    <PersistGate loading={<p>loading...</p>} persistor={pstore}>
+      <App />
+    </PersistGate>
   </Provider>,
   document.getElementById('root')
 );
